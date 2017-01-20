746690e818ae7bb97631fac453eb365746b1f205
Chris Kenst <ckenst@gmail.com>
2017-01-19 20:20:08 -0800

    Initial selenium test to verify page loads

---
+++ tests/site_running.rb
@@ -0,0 +1,23 @@
+require 'selenium-webdriver'
+require 'rspec/expectations'
+include ::RSpec::Matchers
+
+def setup
+  @driver = Selenium::WebDriver.for :chrome
+end
+
+def teardown
+  @driver.quit
+end
+
+def run
+  setup
+  yield
+  teardown
+end
+
+run do
+  @driver.get 'http://testingconferences.org/'
+  heading_text = @driver.find_element(class: 'site-title').text
+  expect(heading_text).to eql 'Software Testing Conferences'
+end
